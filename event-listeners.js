d80d47be175bb0f6014e56fe524ad41fb3112c86
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-06-13 11:30:14 -0500

    listeners skeleton

---
+++ event-listeners.js
@@ -0,0 +1,29 @@
+
+//Listener for enter/return in the message field
+var messageReturn = document.getElementById("").addEventListener("keypress", function(e) {
+	if (e.code === 13) {
+		//load message code
+	}
+});
+
+//Listener for delete all messages
+var deleteMessages = document.getElementById("").addEventListener("click", function(e) {
+	//delete all current messages
+});
+
+//Listener for checkboxes
+var darkBox = document.getElementById("").addEventListener("click", function(e) {
+	//toggle code between (dark gray background, white font) and (white background, black font (default))
+});
+
+var largeBox = document.getElementById("").addEventListener("click", function(e) {
+	//
+})
+
+//Listener for delete last message
+var deleteButton = document.getElementById("").addEventListener("click", function(e) {
+	//delete last message
+})
+
+
+
