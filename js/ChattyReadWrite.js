4128b5cca1a6571bd4fde95db8b82dfd160232f6
Tim Creasy <timcreasy@me.com>
2016-06-14 12:59:56 -0500

    Added timestamps and user placeholder

--- js/ChattyReadWrite.js
+++ js/ChattyReadWrite.js
@@ -11,10 +11,40 @@ var Chatty = (function(Chatty){
       // Create a newMessage div
       var newMessage = document.createElement("div");
 
+      // Timestamp
+      var time = new Date();
+      var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      var curYear = time.getFullYear();
+      var curMonth = months[time.getMonth()];
+      var curDay = time.getDay();
+      var curHour = time.getHours();
+      var curMinute = time.getMinutes();
+      var curSeconds = time.getSeconds();
+      var curMillseconds = time.getMilliseconds();
+      var suffix = null;
+      // Determine AM or PM suffix based on the hour
+      suffix = ( curHour < 12 ) ? "AM" : "PM";
+      // Convert hour from military time
+      curHour = ( curHour < 12 ) ? curHour : curHour - 12;
+      // If hour is 0, set it to 12
+      curHour = curHour || 12;
+      // If seconds and minutes are less than 10, add a zero
+      if ( curMinute < 10 ) {
+        curMinute = "0" + curMinute;
+      }
+      if ( curSeconds < 10) {
+        curSeconds = "0" + curSeconds;
+      }
+      
+      var timestamp = `${curMonth} ${curDay}, ${curYear} at ${curHour}:${curMinute}:${curSeconds}:${curMillseconds} ${suffix}`;
+      console.log(timestamp);
+
       // Add to new message
       newMessage.innerHTML = `<div id="message--${id}" class="message">
                                 <p class="messageText">${message}</p>
                                 <h6 class="messageID">${id}.</h6>
+                                <h6 class="userID">Sent by User on</h6>
+                                <h6 class="timestamp">${timestamp}</h6>
                                 <button id="deleteBtn--${id}" class="deleteButton">Delete Message</button>
                               </div>`;
 
