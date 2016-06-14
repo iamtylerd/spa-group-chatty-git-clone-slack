9d96d057373f03ad847fa8a5ed2185a2e18f3542
Tim Creasy <timcreasy@me.com>
2016-06-14 12:22:41 -0500

    Added check for message present

--- js/ChattyReadWrite.js
+++ js/ChattyReadWrite.js
@@ -2,22 +2,25 @@ var Chatty = (function(Chatty){
 
   Chatty.writeMessageToDOM = function(id, message) {
 
-    // Get messagesContainer
-    var messagesContainer = document.getElementById("messagesContainer");
+    if (message === "") {
+      alert("Please enter a message to add");
+    } else {
+      // Get messagesContainer
+      var messagesContainer = document.getElementById("messagesContainer");
 
-    // Create a newMessage div
-    var newMessage = document.createElement("div");
+      // Create a newMessage div
+      var newMessage = document.createElement("div");
 
-    // Add to new message
-    newMessage.innerHTML = `<div id="message--${id}" class="message">
-                              <p class="messageText">${message}</p>
-                              <h6 class="messageID">${id}.</h6>
-                              <button id="deleteBtn--${id}" class="deleteButton">Delete Message</button>
-                            </div>`;
+      // Add to new message
+      newMessage.innerHTML = `<div id="message--${id}" class="message">
+                                <p class="messageText">${message}</p>
+                                <h6 class="messageID">${id}.</h6>
+                                <button id="deleteBtn--${id}" class="deleteButton">Delete Message</button>
+                              </div>`;
 
-    // Append newMessage to messagesContainer
-    messagesContainer.appendChild(newMessage);
-
+      // Append newMessage to messagesContainer
+      messagesContainer.appendChild(newMessage);
+  }
   };
 
   Chatty.readAllMessages = function() {
