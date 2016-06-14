ff8873539da984c92e25153866e3ceefa8c455a8
iamtylerd <tylerdanielphotography@gmail.com>
2016-06-14 15:01:09 -0500

    added users

--- js/users.js
+++ js/users.js
@@ -1,8 +1,49 @@
+var Chatty = (function (Chatty) {
+
+
 var users = {
-  names: ["Xavier", "Joanna", "Mackenzie", "Gunter", "Iveta", "Sven"]
+  names: ["","Create New", "Xavier", "Joanna", "Mackenzie", "Gunter", "Iveta", "Sven"]
+};
+var selectedUserValue = "";
+
+Chatty.populateUsers = function () {
+    var selectElement = document.getElementById('userSelect');
+    selectElement.innerHTML = "";
+
+for (var i = 0; i < users.names.length; i++) {
+    var newOption = document.createElement("option");
+    newOption.text = users.names[i];
+    selectElement.appendChild(newOption);
+  }
 };
 
-for (var i = 0; i < users.length; i++) {
-    document.getElementbyId('#userSelect').innerHTML +=('<option value="' + users.names.value + '">' + users.names[i] + '</option>');
-    console.log(users);
-  }
+var selectedUser = document.getElementById("userSelect")
+selectedUser.addEventListener("change", getUserValue);
+	function getUserValue () {
+	if (selectedUser.value === "Create New") {
+		$('#addUserModal').modal('show');
+	}
+	else {
+	selectedUserValue = selectedUser.value;
+	console.log(selectedUserValue);
+	}
+};
+
+var newNameInput = document.getElementById("addNameInput");
+var newNameValue = newNameInput.value;
+var newNameButton = document.getElementById("nameSubmit");
+
+newNameButton.addEventListener("click", pushNewName)
+
+function pushNewName () {
+	users.names.push(newNameInput.value);
+	Chatty.populateUsers();
+	$('#addUserModal').modal('hide');
+};
+
+
+
+
+
+return Chatty;
+})(Chatty || {});
